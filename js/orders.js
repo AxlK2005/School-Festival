8f43fb8c8dfeb60533c692334bbe08b15a5c0c34
AxlK2005 <axlk9669@gmail.com>
2024-11-14 15:20:40 +0900

    reconstruct

---
+++ js/orders.js
@@ -0,0 +1,17 @@
+const Orders = [
+    {
+        payment: 'Cash',
+        item: 'ヤキトリ',
+        total: '¥500',
+    },
+    {
+        payment: 'Ticket',
+        item: '焼き鳥',
+        total: '¥1500',
+    },
+    {
+        payment: 'Advance',
+        item: 'やきとり',
+        total: '¥1000',
+    },
+]
